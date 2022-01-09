388015eac8714083c4f9bdd37239a584c4488de7
Delaynie Wheeler <delaynie93@gmail.com>
2022-01-09 15:43:39 -0600

    start to add a bit of custom styling and more sketching options including an eraser button

--- js/main.js
+++ js/main.js
@@ -1,68 +1,63 @@
 const container = document.querySelector('.container')
 const btnBlack = document.createElement('button')
 const btnGrey = document.createElement('button')
+const btnSkittles = document.createElement('button')
 const btnRGB = document.createElement('button')
-const btnSize = document.createElement('button')
+const btnResize = document.createElement('button')
+const btnEraser = document.createElement('button')
+//const btnReset = document.createElement('button')
 const buttonsContainer = document.querySelector('.buttons')
 
 // function to resize the grid
 function resizeGrid() {
-    btnSize.textContent = 'Change Grid Size';
-    btnSize.addEventListener('click', () => {
-        let user = prompt(`What size grid do you want to work with?`);
+    btnResize.textContent = 'Change Size';
+    btnResize.addEventListener('click', () => {
+        let user = prompt(`Put in a number between 1 and 64 to set size, but the grid will default to 16 if the number is too big, too small (0), or no answer is given.`);
         if(user === null || user < 1) {
             reset()
             createDiv(16,16)
             blackColor()
             greyColor()
+            skittlesColor()
             rgbColor()
+            eraser()
         } else {
             reset()
             createDiv(user, user)
             blackColor()
             greyColor()
+            skittlesColor()
             rgbColor()
+            eraser()
         }
     })
-    buttonsContainer.appendChild(btnSize).classList.add('btn');
+    buttonsContainer.appendChild(btnResize).classList.add('btn');
 }
 resizeGrid()
+
 // function create divs
 function createDiv(column, row) {
     
     for(let i = 0; i < (column * row); i++) {
         const div = document.createElement('div');
-        div.style.border = '1px solid'
+        //div.style.border = '1px solid'
         container.style.gridTemplateColumns = `repeat(${column}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${row}, 1fr)`;
         container.appendChild(div).classList.add('box');
     }
 }
 
-createDiv(16,16);
+createDiv(16, 16);
 
-// creating gray button
-
-function greyColor() {
-    const boxes = container.querySelectorAll('.box');
-    btnGrey.textContent = '50 Shades of Grey'
-    btnGrey.addEventListener('click' , () =>{
-        boxes.forEach(box => box.addEventListener('mouseover', () => {
-            let rand = Math.floor(Math.random() * 255)
-            box.style.background = `rgb(${rand}, ${rand}, ${rand})`;
-        }))
-    })
-    
-    buttonsContainer.appendChild(btnGrey).classList.add('btn')
-}
-greyColor()
-
+// function for black 
 function blackColor() {
     const boxes = container.querySelectorAll('.box');
-    btnBlack.textContent = 'Pitch Black'
+    btnBlack.textContent = 'Pitch Black';
     btnBlack.addEventListener('click' , () =>{
         boxes.forEach(box => box.addEventListener('mouseover', () => {
             box.style.background = 'black';
+            box.style.borderRadius = '0';
+
         }))
     })
     
@@ -71,17 +66,58 @@ function blackColor() {
 
 blackColor()
 
-function rgbColor() {
+// creating gray button
+
+function greyColor() {
     const boxes = container.querySelectorAll('.box');
-    btnRGB.textContent = 'Skittles Mode'
-    btnRGB.addEventListener('click' , () =>{
+    btnGrey.textContent = '50 Shades of Grey';
+    btnGrey.addEventListener('click' , () =>{
+        boxes.forEach(box => box.addEventListener('mouseover', () => {
+            let rand = Math.floor(Math.random() * 255)
+            box.style.background = `rgb(${rand}, ${rand}, ${rand})`;
+            box.style.borderRadius = '0';
+
+        }))
+    })
+    
+    buttonsContainer.appendChild(btnGrey).classList.add('btn')
+}
+greyColor()
+
+// function for skittles mode
+function skittlesColor() {
+    const boxes = container.querySelectorAll('.box');
+    btnSkittles.textContent = 'Skittles Mode'
+    btnSkittles.addEventListener('click' , () =>{
         boxes.forEach(box => box.addEventListener('mouseover', () => {
             let red = Math.floor(Math.random() * 255);
             let blue = Math.floor(Math.random() * 255);
             let green = Math.floor(Math.random() * 255);
             
             box.style.background = `rgb(${red},${blue},${green})`
+            box.style.borderRadius = '50px';
             
+        }))
+    })
+    
+    buttonsContainer.appendChild(btnSkittles).classList.add('btn')
+}
+
+skittlesColor()
+
+// function for rainbow mode
+function rgbColor() {
+    const boxes = container.querySelectorAll('.box');
+    btnRGB.textContent = 'Rainbow'
+    btnRGB.addEventListener('click' , () =>{
+        boxes.forEach(box => box.addEventListener('mouseover', () => {
+            let red = Math.floor(Math.random() * 255);
+            let blue = Math.floor(Math.random() * 255);
+            let green = Math.floor(Math.random() * 255);
+            
+            box.style.background = `rgb(${red},${blue},${green})`;
+            box.style.borderRadius = '0';
+           
         }))
     })
     
@@ -90,8 +126,38 @@ function rgbColor() {
 
 rgbColor()
 
-// function to reset grid
+// function for eraser
+
+function eraser() {
+    const boxes = container.querySelectorAll('.box');
+    btnEraser.textContent = 'Eraser'
+    btnEraser.addEventListener('click' , () =>{
+        boxes.forEach(box => box.addEventListener('mouseover', () => {
+            box.style.background = 'white';
+            box.style.borderRadius = '0';
+
+        }))
+    })
+    
+    buttonsContainer.appendChild(btnEraser).classList.add('btn')
+}
+
+eraser()
+
+// function to reset grid after new grid input
 function reset() {
     const boxes = container.querySelectorAll('.box');
     boxes.forEach(box => box.remove())
 }
+
+// function to clear grid
+/*
+function reset() {
+    const boxes = container.querySelectorAll('.box');
+    btnReset.textContent = 'Shake It Up!'
+    btnReset.addEventListener('click' , () => {
+    boxes.forEach(box => box.remove())
+})
+    buttonsContainer.appendChild(btnReset).classList.add('btn');
+}
+reset() */
